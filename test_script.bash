2cabe60ba1fdeb20b3a9243c46221b820fdc2b75
hongjuIm <hongjuim113@gmail.com>
2024-11-12 14:13:53 -0600

    Updated test_script.bash with new code

--- test_script.bash
+++ test_script.bash
@@ -1,6 +1,8 @@
 #!/bin/bash 
 read -p "Enter your name: " name
-if [ "$name" == "Hongju" ]; then
+name=$(echo "$name" | tr '[:upper:]' '[:lower:]')  # Convert input to lowercase
+
+if [ "$name" == "hongju" ]; then
     echo "Valid name"
 else
     echo "Invalid name"
