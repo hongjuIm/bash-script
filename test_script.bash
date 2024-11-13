d286542f0ff9fbd26749d58e37770f6baff018bf
hongjuIm <hongjuim113@gmail.com>
2024-11-13 14:09:50 -0600

    Updated with new code

--- test_script.bash
+++ test_script.bash
@@ -1,9 +1,21 @@
-#!/bin/bash 
-read -p "Enter your name: " name
-name=$(echo "$name" | tr '[:upper:]' '[:lower:]')
+#!/bin/bash
 
-if [ "$name" == "hongju" ]; then
-    echo "Valid name"
-else
-    echo "Invalid name"
+attempts=0  
+
+while [ $attempts -lt 3 ]; do
+    read -p "Enter your name: " name
+    name=$(echo "$name" | tr '[:upper:]' '[:lower:]')  
+    if [ "$name" == "hongju" ]; then
+        echo "Valid name"
+        break  
+    else
+        echo "Invalid name"
+        ((attempts++))  
+        echo "Attempt $attempts of 3"
+    fi
+done
+
+if [ $attempts -eq 3 ]; then
+    echo "Out of attempts"
 fi
+
